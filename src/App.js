f0fdf43bc753a4a3239acfe7419c46290a08e71d
emthedm1 <emartin2@instructors.2u.com>
2022-05-03 20:04:42 -0600

    added db.json file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wordle</h1>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+data we need to track:
+  -- solution
+    -- 5 letter string, e.g. 'arise'
+  -- past guesses
+    -- an array of past guesses
+    -- each past guess is an array of letter objects [{}, {}, {}, {}, {}]
+    -- each object represents a letter in the guess word {letter: 'a', color: 'yellow'}
+    -- current guess
+      -- string 'hello'
+    -- keypad letters
+      -- array of letter objects [{key: 'a', color: 'green'}, {}, {} ...]
+    -- number of turns
+      -- an integer 0 - 6
+
+game process:
+  -- entering words:
+    -- user enters a letter & a square is filled with that letter
+    -- when a user hits delete it deletes the previous letter
+    -- when a user hits enter it submits the word
+      -- if all squares are not filled with letters then the word is not submitted
+      -- if that word has already been used in a previous guess then the word is not submitted
+      -- if that word does not exist in the dictionary then the word is not submitted
+  -- checking submitted words:
+    -- each letter is checked to see if it matches to the solution
+    -- each letter is assigned a color based on its inclusion in the solution
+      -- exact matches (correct position in the solution) are green
+      -- partial matches (in the solution but not the correct position) are yellow
+      -- non matches (not in the solution at all) are gray
+    -- the guess is added to the grid with the correct colors
+    -- the current guess moves to the next row
+    -- the keypad letters are updated (colors)
+  -- ending the game:
+    -- when the guessed word fully matches the solution
+      -- modal to say 'well done'
+    -- when the user runs out of guesses
+      -- modal to say 'unlucky'
+    
+*/
+
+// solution date:
+// 1 - 3rd party api
+// 2 - own database (e.g. mongodb)
+// 3 - json file
