bb6b4801b6de7fbd994db5bbd03b4aac504107a0
emthedm1 <emartin2@instructors.2u.com>
2022-05-03 23:31:32 -0600

    added to custom hook

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
 
-export default function Wordle() {
-  return <div>Wordle</div>;
+export default function Wordle({ solution }) {
+  const { currentGuess, handleKeyup } = useWordle(solution);
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
+
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup]);
+
+  return <div>current guess - {currentGuess}</div>;
 }
